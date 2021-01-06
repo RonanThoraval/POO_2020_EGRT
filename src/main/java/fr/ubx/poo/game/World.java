5e29cbcb80ec210bea3e00428373cb682a6b2bec
Ronan <ronan.thoraval@etu.u-bordeaux.fr>
2021-01-06 21:54:16 +0100

    qUELQUES PETITES MODIFS

--- src/main/java/fr/ubx/poo/game/World.java
+++ src/main/java/fr/ubx/poo/game/World.java
@@ -16,7 +16,7 @@ import java.util.function.BiConsumer;
 
 public class World {
     private final Map<Position, Decor> grid;
-    private final WorldEntity[][] raw;
+    public final WorldEntity[][] raw;
     public final Dimension dimension;
     private boolean changed=true;
 
